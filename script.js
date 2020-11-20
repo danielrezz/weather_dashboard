3c7d202e7b1d1f986f7cdee8498c0bc3a8f7fc72
Daniel Reza <daniel.reza1007@gmail.com>
2020-11-19 20:35:15 -0600

    added global variables

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 $( document ).ready(function() {
+    var APIKey = "186950d5261992f795f01a481c7fd390";
+    var city = $("#city-input").val().trim();
+
+    var weatherQueryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
+    var forecastQueryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial";
+    // figure out lat & lon
+    var uviQueryURL = "http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}"; 
+
     
-
-
-
-
 
 
 
