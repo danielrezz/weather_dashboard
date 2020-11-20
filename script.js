35f83fda05938553da391f8ff720754754258b84
Daniel Reza <daniel.reza1007@gmail.com>
2020-11-19 23:14:37 -0600

    updating and calling it a night

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ $( document ).ready(function() {
     var city = $("#city-input").val().trim();
     var cities = [];
 
-    var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
-    var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial";
+    let weatherQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
+    var forecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial";
     // figure out lat & lon
     var uviQueryURL = "http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}"; 
 
@@ -14,14 +14,31 @@ $( document ).ready(function() {
     $.ajax({
         url: weatherQueryURL,
         method: "GET"
-      })
-      .then(function(response) {
-
+      }).then(function(response) {
+       
      var cityName = $("#city-name").text(response.name);
 
-     $("<ul>").append(cityName);
+     $("#current-conditions").append(cityName);
 
     });
+
+// function to render buttons
+
+    function renderButtons() {
+
+        $("#city-buttons").empty();
+
+        for (var i = 0; i < cities.length; i++) {
+
+            var c = $("<button>");
+            c.addClass("city-btn");
+            c.attr("data-name", cities[i]);
+            c.text(cities[i]);
+            $("#city-buttons").append(c);
+            console.log(cities);
+
+        }
+    }
 
     // event function when button is clicked
     $("#button-addon2").on("click", function(event) {
@@ -31,12 +48,11 @@ $( document ).ready(function() {
         var city = $("#city-input").val().trim();
 
         cities.push(city);
-        console.log(cities);
 
-    // need to call function here!!!
+    renderButtons();
 
     })
 
-
+    renderButtons();
 
 });
