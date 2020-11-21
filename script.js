3a41762a049cbc256359189a92f1b4eab87d9901
Daniel Reza <daniel.reza1007@gmail.com>
2020-11-20 21:15:15 -0600

    adding cards for 5 day forecast

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ $( document ).ready(function() {
         var APIKey = "186950d5261992f795f01a481c7fd390";
         var city = $("#city-input").val().trim();
         var weatherQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
+        var forecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial";
         var date = moment().format("L");
 
 // current weather API
@@ -27,7 +28,7 @@ $( document ).ready(function() {
      var icon = $("#icon").attr("src", "https://api.openweathermap.org/img/w/" + response.weather[0].icon + ".png");
      $("#current-conditions").append(icon);
 
-     var temperature = $("#temperature").text("Temperature: " + response.main.temp + "°");
+     var temperature = $("#temperature").text("Temperature: " + response.main.temp + "°F");
      $("#current-conditions").append(temperature);
 
      var humidity = $("#humidity").text("Humidity: " + response.main.humidity);
@@ -37,6 +38,50 @@ $( document ).ready(function() {
      $("#current-conditions").append(windSpeed);
 
     });
+
+// 5 day forecast
+
+    $.ajax({
+        url: forecastQueryURL,
+        method: "GET"
+      }).then(function(response) {
+
+        console.log(response);
+
+        var day1 = response.list[5];
+        var day2 = response.list[13];
+        var day3 = response.list[21];
+        var day4 = response.list[29];
+        var day5 = response.list[37];
+        var days = [day1, day2, day3, day4, day5];
+
+        for (var i = 0; i < days.length; i++) {
+
+            var d = $("<div></div>");
+            d.addClass("card bg-light mb-3");
+            d.width("18rem");
+            $(".forecast-div").append(d);
+            var t = $("<h4></h4>");
+            t.text(days[i].dt_txt.slice(0, 10));
+            d.append(t);
+            var dayIcon = $("<img>");
+            dayIcon.attr("src", "https://api.openweathermap.org/img/w/" + days[i].weather[0].icon + ".png");
+            dayIcon.width("80px");
+            d.append(dayIcon);
+            var p = $("<p></p>");
+            p.addClass("card-text");
+            p.text("Temp: " + days[i].main.temp_max + "°F");
+            d.append(p);
+            var p2 = $("<p></p>");
+            p2.addClass("card-text");
+            p2.text("Humidity: " + days[i].main.humidity + "%");
+            d.append(p2);
+
+        }
+
+      });
+
+
     };
 
 // function to render buttons
@@ -58,13 +103,11 @@ $( document ).ready(function() {
 
     // event function when button is clicked
     $("#button-addon2").on("click", function(event) {
+        event.preventDefault();
 
         retrieveCityInfo();
 
-        event.preventDefault();
-
         var city = $("#city-input").val().trim();
-
         cities.push(city);
 
     renderButtons();
